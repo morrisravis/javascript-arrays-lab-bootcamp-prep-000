f0b8224ffbd948c199f3754bab3b0803ab05081e
Morris Ravis <morrisravis@berkeley.edu>
2018-07-24 18:51:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function destructivelyAppendKitten(name) { kittens.push("Ralph");
 function destructivelyPrependKitten(name) { kittens.unshift("Bob");
 }
 
-function destructivelyRemoveLastKitten() { kitten.pop();
-return kittens
+function destructivelyRemoveLastKitten() {
+kittens.pop();
+return kittens;
 }
