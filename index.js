4b8d307c20365a3c10218bc479209b7ff1945e7d
Morris Ravis <morrisravis@berkeley.edu>
2018-07-24 19:18:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,8 +28,15 @@ newKittens.unshift(name);
 return newKittens;
 }
 
-function removeLastKitten() { 
-var newKittens = [...kittens];
-newKittens.pop();
-return new kittens;
+function removeLastKitten() {
+var newKittens = [...kittens]
+newKittens.pop()
+return newKittens
+}
+
+function removeFirstKitten() {
+var newKittens = [...kittens]
+newKittens.shift()
+return newKittens
+}
 }
